cb1348ce34b3b4e91613886aef113c4c6686ed91
Marc Dougherty <muncus@gmail.com>
2016-08-08 14:02:03 -0700

    simple error service that produces error 451 "Censored"

---
+++ errorservice/simple.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby2.0
+# Server side of muni display. Serves simple number of mins to next train.
+# Configuration file maps a url path to a set of nextbus query options. 
+# see the full nextbus api spec linked from README.md for available options.
+require 'rubygems'
+require 'bundler/setup'
+
+require 'sinatra'
+
+set :bind, '0.0.0.0'
+disable :show_exceptions
+
+get '*' do |p|
+  status 451
+  headers \
+    'X-filtered-by' => "Internet Volume Control 1.0"
+  body "This url has been filtered by Internet Volume Control."
+end
